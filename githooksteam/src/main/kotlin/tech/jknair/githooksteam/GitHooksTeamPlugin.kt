9485b24fac6eaeefcbec92a254c0d7a99c3d6772
jk <jayakrishnan.nair@byjus.com>
2020-06-21 21:43:10 +0530

    fix: removed unnecessary type declarations

--- githooksteam/src/main/kotlin/tech/jknair/githooksteam/GitHooksTeamPlugin.kt
+++ githooksteam/src/main/kotlin/tech/jknair/githooksteam/GitHooksTeamPlugin.kt
@@ -12,10 +12,10 @@ open class GitHooksTeamExtension {
 open class GitHooksTeamPlugin : Plugin<Project> {
 
     override fun apply(project: Project) {
-        val extension = project.extensions.create<GitHooksTeamExtension>(PLUGIN_NAME, GitHooksTeamExtension::class.java)
+        val extension = project.extensions.create(PLUGIN_NAME, GitHooksTeamExtension::class.java)
         val srcDir = extension.srcDir ?: getDefaultSrcDir(project)
         val destDir = getHooksDir(project)
-        project.tasks.create<Copy>(TASK_NAME, Copy::class.java) {
+        project.tasks.create(TASK_NAME, Copy::class.java) {
             from(srcDir)
             into(destDir)
             doLast {
@@ -23,7 +23,7 @@ open class GitHooksTeamPlugin : Plugin<Project> {
                 createAddFileExecPermissionTask(project, destDir)
             }
         }
-        if(project.tasks.findByName("build") != null) {
+        if (project.tasks.findByName("build") != null) {
             project.tasks.named("build") {
                 dependsOn(TASK_NAME)
             }
@@ -34,12 +34,12 @@ open class GitHooksTeamPlugin : Plugin<Project> {
         return "${project.rootDir}/.githooks"
     }
 
-    private fun getHooksDir(project: Project): String{
+    private fun getHooksDir(project: Project): String {
         return "${project.rootDir}/.git/hooks"
     }
 
     private fun createAddFileExecPermissionTask(project: Project, dirPath: String): Exec {
-        return project.tasks.create<Exec>("addExecutePermission", Exec::class.java) {
+        return project.tasks.create("addExecutePermission", Exec::class.java) {
             project.file(dirPath).listFiles()?.forEach { file ->
                 val filePathStr = file.absolutePath
                 commandLine("chmod", "+x", filePathStr)
